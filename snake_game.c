ec35265d37f23e983c9dfc0d7fe0c2e5c5064049
Lucas Duarte Correia <84639763+lucasz-dc@users.noreply.github.com>
2021-07-12 22:15:14 -0300

    Update snake_game.c

--- snake_game.c
+++ snake_game.c
@@ -6,21 +6,27 @@ const int width = 50;
 void game_screen(){
 
     for (int i=0; i < width; i++){
+        
         printf("%c", 176);
+        
     }
 
     printf("\n");
 
     for(int i = 0; i < height; i++){
+        
         for(int j = 0; j < width; j++){
 
             if(j == 0){
+                
                 printf("%c", 176);
 
             }else if (j ==  width - 1){
+                
                 printf("%c", 176);
 
             }else{
+                
                 printf(" ");
             }
 
